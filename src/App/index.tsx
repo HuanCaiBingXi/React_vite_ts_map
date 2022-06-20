352a376fb39f6bfd107e45c8c1cbdf5c51060b2c
幻彩冰袭 <1994370030@qq.com>
2022-06-20 12:47:24 +0800

    init

---
+++ src/App/index.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import "./App.less";
+
+
+export const App = () => {
+    return <div>App</div>;
+};
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import postcssImport from "postcss-import";
+import autoprefixer from "autoprefixer";
+import lessPluginAutoprefix from "less-plugin-autoprefix";
+import path from "path";
+//vite.config.ts
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src"),
+        },
+    },
+    css: {
+        modules: {
+            generateScopedName: `[name]__[local]__[hash:base64:5]`,
+            hashPrefix: "prefix",
+        },
+        preprocessorOptions: {
+            less: {
+                javascriptEnable: true,
+            },
+        },
+        postcss: {
+            plugins: [postcssImport, autoprefixer, lessPluginAutoprefix],
+        },
+    },
+});
